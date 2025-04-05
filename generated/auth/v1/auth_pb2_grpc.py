41103a7df8bcbf67d9b0ad6698bf451eb6e84450
Андрей Васильев <wotacc0809@gmail.com>
2025-04-05 16:51:27 +0500

    fix

--- generated/auth/v1/auth_pb2_grpc.py
+++ generated/auth/v1/auth_pb2_grpc.py
@@ -3,6 +3,7 @@
 import grpc
 import warnings
 
+from . import auth_pb2 as auth_dot_v1_dot_auth__pb2
 
 GRPC_GENERATED_VERSION = '1.71.0'
 GRPC_VERSION = grpc.__version__
@@ -33,14 +34,30 @@ class AuthStub(object):
         Args:
             channel: A grpc.Channel.
         """
+        self.AuthorizeByToken = channel.unary_unary(
+                '/auth.Auth/AuthorizeByToken',
+                request_serializer=auth_dot_v1_dot_auth__pb2.AuthorizeByTokenRequest.SerializeToString,
+                response_deserializer=auth_dot_v1_dot_auth__pb2.AuthorizeByTokenResponse.FromString,
+                _registered_method=True)
 
 
 class AuthServicer(object):
     """Missing associated documentation comment in .proto file."""
 
+    def AuthorizeByToken(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_AuthServicer_to_server(servicer, server):
     rpc_method_handlers = {
+            'AuthorizeByToken': grpc.unary_unary_rpc_method_handler(
+                    servicer.AuthorizeByToken,
+                    request_deserializer=auth_dot_v1_dot_auth__pb2.AuthorizeByTokenRequest.FromString,
+                    response_serializer=auth_dot_v1_dot_auth__pb2.AuthorizeByTokenResponse.SerializeToString,
+            ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
             'auth.Auth', rpc_method_handlers)
@@ -51,3 +68,30 @@ def add_AuthServicer_to_server(servicer, server):
  # This class is part of an EXPERIMENTAL API.
 class Auth(object):
     """Missing associated documentation comment in .proto file."""
+
+    @staticmethod
+    def AuthorizeByToken(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(
+            request,
+            target,
+            '/auth.Auth/AuthorizeByToken',
+            auth_dot_v1_dot_auth__pb2.AuthorizeByTokenRequest.SerializeToString,
+            auth_dot_v1_dot_auth__pb2.AuthorizeByTokenResponse.FromString,
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)
